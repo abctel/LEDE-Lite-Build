d7bbabb67e062a0075df4c049d3cb07ca34136ac
abctel <abctel@gmail.com>
2021-04-27 16:00:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,24 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.20.200/g' package/base-files/files/bin/config_generate
+
+# 增加微信推送姬
+git clone https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
+
+# 增加dockerman
+git clone https://github.com/lisaac/luci-app-dockerman package/lean/luci-app-dockerman
+rm -rf package/lean/luci-app-dockerman/.github
+rm -rf package/lean/luci-app-dockerman/doc
+rm package/lean/luci-app-dockerman/LICENSE
+rm package/lean/luci-app-dockerman/README.md
+cp -rf package/lean/luci-app-dockerman/applications/luci-app-dockerman package/lean
+
+# 增加一套主题
+#git clone https://github.com/siropboy/luci-theme-btmod package/lean/luci-theme-btmod
+
+# 删除老argon主题
+rm -rf package/lean/luci-theme-argon
+# 拉取argon主题
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
