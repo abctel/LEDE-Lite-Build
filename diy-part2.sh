0e5e4205e7701b54ce41504e56fe097476db0731
abctel <abctel@gmail.com>
2022-04-09 11:10:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,16 +13,16 @@
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.100.200/g' package/base-files/files/bin/config_generate
 
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/abctel/luci-app-smartdns
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/abctel/luci-app-adguardhome
-git clone https://github.com/xiaorouji/openwrt-passwall package/abctel/openwrt-passwall
-git clone https://github.com/abctel/luci-theme-edge package/abctel/luci-theme-edge
-git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/abctel/luci-app-chinadns-ng
-git clone https://github.com/abctel/abctel-package package/abctel/dnsmasq-china-list
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/abctel/luci-app-smartdns
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/abctel/luci-app-adguardhome
+#git clone https://github.com/xiaorouji/openwrt-passwall package/abctel/openwrt-passwall
+#git clone https://github.com/abctel/luci-theme-edge package/abctel/luci-theme-edge
+#git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/abctel/luci-app-chinadns-ng
+#git clone https://github.com/abctel/abctel-package package/abctel/dnsmasq-china-list
 #git clone https://github.com/ITdesk01/jd_openwrt_script.git package/abctel/jd_openwrt_script
 
-chmod 755 files/etc/init.d/smartdns
-chmod 755 files/usr/bin/AdGuardHome/AdGuardHome
-rm -rf feeds/packages/net/dnscrypt-proxy2
-git clone https://github.com/abctel/dnscrypt-proxy2 feeds/packages/net/dnscrypt-proxy2
-make defconfig
+#chmod 755 files/etc/init.d/smartdns
+#chmod 755 files/usr/bin/AdGuardHome/AdGuardHome
+#rm -rf feeds/packages/net/dnscrypt-proxy2
+#git clone https://github.com/abctel/dnscrypt-proxy2 feeds/packages/net/dnscrypt-proxy2
+#make defconfig
